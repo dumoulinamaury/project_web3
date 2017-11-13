75457af1ef26957f3bda4a4825e69250cd60d364
d.amaury <d.amaury@hotmail.com>
2017-11-13 21:20:41 +0100

    nav via config/nav.yml

--- src/JemaBundle/Controller/NavController.php
+++ src/JemaBundle/Controller/NavController.php
@@ -4,15 +4,16 @@ namespace JemaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Finder\Finder;
+
 
 class NavController extends Controller
 {
     public function _navAction()
     {
-        $nav[] = ["name" => "index", "access" => "public", "pages" => [
-                        ["name" => "index", "access" => "public", "route" => "index"],
-                        ["name" => "page2", "access" => "public", "route" => "index"]
-                    ]];
+        $nav = Yaml::parse(file_get_contents('../src/JemaBundle/Resources/config/nav.yml'));
+
         return $this->render('nav.html.twig', [
             'nav' => $nav
         ]);
